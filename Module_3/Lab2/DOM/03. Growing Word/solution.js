edc1fe2dd35201e32188c15498876c5e590f62b3
Patrick Galloway <patrick@kingslanduniversity.com>
2021-08-04 09:25:08 -0400

    finished DOM Lab Questions

--- Module_3/Lab2/DOM/03. Growing Word/solution.js
+++ Module_3/Lab2/DOM/03. Growing Word/solution.js
@@ -1,4 +1,32 @@
 function growingWord() {
+  let growingText = document.getElementById("exercise").lastElementChild;  
+  
+  //console.log(growingText.style.fontSize );
 
-  //TODO...
+  if(growingText.style.fontSize== ""&&growingText.style.color == ""){
+    //console.log("Initial set")
+    growingText.style.fontSize = 2+"px";
+    growingText.style.color = "blue";
+  }else if(growingText.style.color == "blue" ){
+    //console.log("blue to green")
+    growingText.style.color = "green";
+    //"2px"
+    let fontSize = growingText.style.fontSize;
+    let size = Number(fontSize.substring(0,fontSize.length-2));
+    growingText.style.fontSize = (size*2)+"px";
+  }else if(growingText.style.color == "green" ){
+    //console.log("green to red")
+    growingText.style.color = "red";
+    //"2px"
+    let fontSize = growingText.style.fontSize;
+    let size = Number(fontSize.substring(0,fontSize.length-2));
+    growingText.style.fontSize = (size*2)+"px";
+  }else if(growingText.style.color == "red" ){
+    //console.log("red to blue")
+    growingText.style.color = "blue";
+    //"2px"
+    let fontSize = growingText.style.fontSize;
+    let size = Number(fontSize.substring(0,fontSize.length-2));
+    growingText.style.fontSize = (size*2)+"px";
+  }
 }
--- Module_3/Lab2/DOM/04. Visited Websites/solution.js
+++ Module_3/Lab2/DOM/04. Visited Websites/solution.js
@@ -1,4 +1,24 @@
 function solve() {
+  //add event listeners to all of the a tags
+  let aTags = document.getElementsByTagName("a");
 
-  //TODO...
+  let link1 = document.getElementsByClassName("link-1");
+  
+  for(let a of aTags){
+    a.addEventListener("click",()=>{
+      
+      //1. next child in the list
+      // console.log(a);
+      // console.log(a.nextElementSibling);
+      let p = a.nextElementSibling;
+      let textArray = p.innerHTML.split(" ");
+      //console.log(textArray);
+      textArray[1]= Number(textArray[1])+1;
+      p.innerHTML = textArray.join(" ");
+
+      //2. go to parent, get last child
+      // console.log(a.parentElement);
+      // console.log(a.parentElement.lastElementChild);
+    });
+  }
 }
